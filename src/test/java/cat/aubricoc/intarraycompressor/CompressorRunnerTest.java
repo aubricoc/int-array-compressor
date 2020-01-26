6b9328f182fe42ac15eb63edbf10b945a805aca8
Jordi Albert <jordi.albert.m@gmail.com>
2020-01-26 09:37:15 +0100

    Setup

---
+++ src/test/java/cat/aubricoc/intarraycompressor/CompressorRunnerTest.java
@@ -0,0 +1,11 @@
+package cat.aubricoc.intarraycompressor;
+
+import org.junit.jupiter.api.Test;
+
+public class CompressorRunnerTest {
+
+    @Test
+    public void test() {
+        CompressorRunner.main();
+    }
+}
